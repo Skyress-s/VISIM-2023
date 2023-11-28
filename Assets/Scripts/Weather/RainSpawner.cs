e4010745831e61d5019ad424c27961ce85117aae
Mathias Mohn Mørch <mathmoe64@gmail.com>
2023-11-28 13:04:44 +0100

    feat: Added rain spawner

--- Assets/Scripts/Weather/RainSpawner.cs
+++ Assets/Scripts/Weather/RainSpawner.cs
@@ -5,12 +5,34 @@ namespace KT {
     public class RainSpawner : MonoBehaviour {
         [SerializeField] private GameObject raindropPrefab;
         
-        
         [SerializeField]  private Vector3 spawnVolumeSize = Vector3.one * 4f;
 
+        [SerializeField] private float spawnFrequency = 1f;
+        private float SpawnPeriod => 1f / spawnFrequency;
 
+        private float spawnTimer = 0f;
+        
         private void OnDrawGizmos() {
             Gizmos.DrawWireCube(transform.position,spawnVolumeSize);
+        }
+
+
+        private void Update() {
+            spawnTimer += Time.deltaTime;
+            if (spawnTimer > SpawnPeriod) {
+                spawnTimer -= SpawnPeriod;
+                SpawnRaindrop();
+                
+            }
+        }
+
+        private void SpawnRaindrop() {
+            Vector3 spawnPos = transform.position + new Vector3(
+                UnityEngine.Random.Range(-spawnVolumeSize.x, spawnVolumeSize.x),
+                UnityEngine.Random.Range(-spawnVolumeSize.y, spawnVolumeSize.y),
+                UnityEngine.Random.Range(-spawnVolumeSize.z, spawnVolumeSize.z)
+            );
+            Instantiate(raindropPrefab, spawnPos, Quaternion.identity);
         }
     }
 }
